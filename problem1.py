28caffdc1fdb34273237774e46b61ac3d6e558cf
isaiah-debug <binghamisaiah177@gmail.com>
2026-05-04 00:13:06 -0700

    update

--- problem1.py
+++ problem1.py
@@ -11,3 +11,6 @@ cur.execute(sql)
 for row in cur.fetchall():
    print('row[0]=', row[0])
    print('row[1]=', row[1])
+
+#line ten throws an exception - no such table users 
+#guessing that when FROM is put in then there must be a table to something after
